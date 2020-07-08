b6c11177ba7cab4dfe78daa4634b655397f224d6
Aleksey Mishin <a.mishnvp@gmail.com>
2020-07-08 19:43:55 +0300

    feature: hello opengl triangle

--- UGame/Source/UGame/Application.cpp
+++ UGame/Source/UGame/Application.cpp
@@ -40,11 +40,103 @@ namespace UGame
 
 	void Application::Run()
 	{
+		const float vertices[] = {
+			-0.5f, -0.5f, 0.f,
+			0.f, 0.5f, 0.f,
+			0.5f, -0.5f, 0.f
+		};
+
+		unsigned int vertexArray;
+		glGenVertexArrays(1, &vertexArray);
+		glBindVertexArray(vertexArray);
+
+		unsigned int vertexBuffer;
+		glCreateBuffers(1, &vertexBuffer);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+		
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		const std::string vertexShaderSrc = R"(
+			#version 330 core
+			layout (location = 0) in vec3 aPos;
+
+			void main()
+			{
+				gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+			})";
+		unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+		const GLchar* vertexSrc = vertexShaderSrc.c_str();
+		glShaderSource(vertexShader, 1, &vertexSrc, NULL);
+		glCompileShader(vertexShader);
+
+		GLint isCompiled = 0;
+		glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &isCompiled);
+		if (isCompiled == GL_FALSE)
+		{
+			GLint maxLength = 0;
+			glGetShaderiv(vertexShader, GL_INFO_LOG_LENGTH, &maxLength);
+
+			std::vector<GLchar> infoLog(maxLength);
+			glGetShaderInfoLog(vertexShader, maxLength, &maxLength, &infoLog[0]);
+
+			glDeleteShader(vertexShader);
+			return;
+		}
+
+		const std::string fragmentShaderSrc = R"(
+		#version 330 core
+		out vec4 FragColor;
+		
+		void main()
+		{
+			FragColor = vec4(1.f, 0.5f, 0.2f, 1.f);
+		})";
+
+		unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+		const GLchar* fragmentSrc = vertexShaderSrc.c_str();
+		glShaderSource(fragmentShader, 1, &fragmentSrc, NULL);
+		glCompileShader(fragmentShader);
+
+		GLint isFragmentCompiled = 0;
+		glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &isFragmentCompiled);
+		if (isCompiled == GL_FALSE)
+		{
+			GLint maxLength = 0;
+			glGetShaderiv(fragmentShader, GL_INFO_LOG_LENGTH, &maxLength);
+
+			std::vector<GLchar> infoLog(maxLength);
+			glGetShaderInfoLog(fragmentShader, maxLength, &maxLength, &infoLog[0]);
+
+			glDeleteShader(vertexShader);
+			glDeleteShader(fragmentShader);
+			return;
+		}
+
+		unsigned int shaderProgram = glCreateProgram();
+		glAttachShader(shaderProgram, vertexShader);
+		glAttachShader(shaderProgram, fragmentShader);
+		glLinkProgram(shaderProgram);
+
+		glUseProgram(shaderProgram);
+
+		glDeleteShader(vertexShader);
+		glDeleteShader(fragmentShader);
+
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(0);
+
+
+
 		while (running)
 		{
 			glClearColor(1, 0, 1, 1);
 			glClear(GL_COLOR_BUFFER_BIT);
 			
+			glDrawArrays(GL_TRIANGLES, 0, 3);
+
+
 			for (Layer* layer : layerStack)
 			{
 				layer->OnUpdate();
